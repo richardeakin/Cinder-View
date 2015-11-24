4b7fe4b0b03dd0d0e76f1557e1e145053e1f8c1e
Rich E <rich.eakin@gmail.com>
2015-11-24 05:18:31 -0500

    working cmake build with libcinder-view.a as dependency of test app

--- cmake/modules/view_sources.cmake
+++ cmake/modules/view_sources.cmake
@@ -6,7 +6,7 @@ list( APPEND VIEW_SOURCES
 	${VIEW_SOURCE_PATH}/view/ImageView.cpp
 	${VIEW_SOURCE_PATH}/view/Interface3d.cpp
 	${VIEW_SOURCE_PATH}/view/Label.cpp
-	${VIEW_SOURCE_PATH}/view/Layer.cpp
+#	${VIEW_SOURCE_PATH}/view/Layer.cpp
 	${VIEW_SOURCE_PATH}/view/ScrollView.cpp
 	${VIEW_SOURCE_PATH}/view/Selector.cpp
 	${VIEW_SOURCE_PATH}/view/Slider.cpp
