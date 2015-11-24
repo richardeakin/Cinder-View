42dd1bf6eb98bb590fcaef8136654ac7f9ccd274
Rich E <rich.eakin@gmail.com>
2015-11-24 05:54:01 -0500

    add Layer.cpp to cmake build

--- cmake/modules/view_sources.cmake
+++ cmake/modules/view_sources.cmake
@@ -6,7 +6,7 @@ list( APPEND VIEW_SOURCES
 	${VIEW_SOURCE_PATH}/ui/ImageView.cpp
 	${VIEW_SOURCE_PATH}/ui/Interface3d.cpp
 	${VIEW_SOURCE_PATH}/ui/Label.cpp
-#	${VIEW_SOURCE_PATH}/ui/Layer.cpp
+	${VIEW_SOURCE_PATH}/ui/Layer.cpp
 	${VIEW_SOURCE_PATH}/ui/ScrollView.cpp
 	${VIEW_SOURCE_PATH}/ui/Selector.cpp
 	${VIEW_SOURCE_PATH}/ui/Slider.cpp
