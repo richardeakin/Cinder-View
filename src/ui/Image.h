43dace25afb1f2f109b575a6b68b9c3d32debb74
Rich E <rich.eakin@gmail.com>
2016-03-10 00:31:55 -0500

    add getter for gl texture

--- src/ui/Image.h
+++ src/ui/Image.h
@@ -49,6 +49,8 @@ class Image {
 	const ci::ivec2&    getSize() const     { return mSize; }
 	ci::Area            getBounds() const   { return ci::Area( 0, 0, mSize.x, mSize.y ); }
 
+	const ci::gl::TextureRef&	getTexture() const	{ return mTexture; }
+
   private:
 	ci::gl::TextureRef	mTexture;
 	ci::ivec2           mSize;
