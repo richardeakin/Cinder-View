4c2bf3492c427720d2341a84879e646d83864855
Rich E <rich.eakin@gmail.com>
2020-12-10 22:04:29 -0500

    template now works with TinderBox, fleshed out the template .cpp

--- templates/Basic Cinder-View/src/_TBOX_PREFIX_App.cpp
+++ templates/Basic Cinder-View/src/_TBOX_PREFIX_App.cpp
@@ -1,37 +1,54 @@
 #include "cinder/app/App.h"
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Log.h"
 
 #include "vu/vu.h"
 
 using namespace ci;
-using namespace ci::app;
 using namespace std;
 
-class _TBOX_PREFIX_App : public App {
+class _TBOX_PREFIX_App : public app::App {
   public:
 	void setup() override;
-	void mouseDown( MouseEvent event ) override;
+	void resize() override;
 	void update() override;
 	void draw() override;
-	
+
+  private:
+	vu::GraphRef mGraph;
 };
 
 void _TBOX_PREFIX_App::setup()
 {
+	mGraph = make_shared<vu::Graph>();
+	mGraph->setFillParentEnabled();
+	mGraph->connectEvents();
+
+	// add a Button
+	auto button = make_shared<vu::Button>( Rectf( 10, 10, 60, 40 ) );
+	button->setTitle( "Button" );
+	button->getSignalPressed().connect( [] { CI_LOG_I( "button pressed" ); } );
+	button->getSignalReleased().connect( [] { CI_LOG_I( "button released" ); } );
+
+	mGraph->addSubview( button );
 }
 
-void _TBOX_PREFIX_App::mouseDown( MouseEvent event )
+void _TBOX_PREFIX_App::resize()
 {
+	mGraph->setNeedsLayout();
 }
 
 void _TBOX_PREFIX_App::update()
 {
+	mGraph->propagateUpdate();
 }
 
 void _TBOX_PREFIX_App::draw()
 {
-	gl::clear();	
+	gl::clear( Color( 0, 0, 0 ) );
+
+	mGraph->propagateDraw();
 }
 
-CINDER_APP( _TBOX_PREFIX_App, RendererGl )
+CINDER_APP( _TBOX_PREFIX_App, app::RendererGl )
